66103c0b7f45a08914c21bce1d1f59e739ddedf1
cryptocoins <crypto-coins@yandex.ru>
2016-11-23 09:00:36 +0300

    Simple block viewer

---
+++ bv.js
@@ -0,0 +1,25 @@
+'use strict'
+
+const blockchain = require('./lib/Blockchain')
+const Block = require('./lib/Block')
+const Tx = require('./lib/Tx')
+
+var id = process.argv[2] || process.exit(0)
+var toId = null
+
+if (id === 'e') {
+  let maxId
+  if (maxId = process.argv[3]) {
+    blockchain.eachTo(maxId, (block) => {
+      console.log(block.id, block.hash.toString('hex'))
+    })
+  } else {
+    blockchain.each((block) => {
+      console.log(block.id, block.hash.toString('hex'))
+    })
+  }
+}
+console.log('Blockchain length:', blockchain.getLength())
+setTimeout(() => {
+    process.exit()
+}, 1000)
