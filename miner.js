3068ebfecc2f2ffdffd25dc558894c560e8b84f3
cryptocoins <crypto-coins@yandex.ru>
2016-12-08 19:02:29 +0300

    Miner update

--- miner.js
+++ miner.js
@@ -5,6 +5,7 @@ const helper = require('./lib/helper')
 const hours = require('./lib/Hours')
 const storage = require('./lib/Storage')
 const Block = require('./lib/Block')
+const Packet = require('./lib/Packet')
 
 storage.config = config
 storage.config.listenRpcPort = 5840
@@ -15,9 +16,29 @@ hours.sync()
 
 console.log('Miner runned')
 
-const HASHES_PER_CYCLE = 100000
+var hashesPerCycle = 100000
 var nonce = 0
 var working = false
+
+const args = process.argv
+for (let i = 2; i < args.length; i++) {
+  if ((args[i] === '--hpc') && (args[i + 1] !== undefined)) {
+    hashesPerCycle = args[i + 1]
+  } else if ((args[i] === '--nonce') && (args[i + 1] !== undefined)) {
+    const buffer = Buffer.alloc(8, 0x00)
+    let argNonce = args[i + 1]
+    if (argNonce.length % 2) {
+      argNonce = '0' + argNonce
+    }
+    const nonceBuffer = Buffer.from(argNonce, 'hex')
+    nonceBuffer.copy(buffer, 8 - nonceBuffer.length)
+    nonce = Packet(buffer).unpackNumber64()
+  }
+}
+
+console.log('Configuration')
+console.log('HPC     :', hashesPerCycle)
+console.log('Nonce   :', nonce)
 
 function continueMining() {
   if (working) {
@@ -49,8 +70,8 @@ function continueMining() {
     
     let hash
     const timeStart = helper.unixTimeMs()
-    for (let i = 0; i < HASHES_PER_CYCLE; i++) {
-      nonce++
+    for (let i = 0; i < hashesPerCycle; i++) {
+      nonce = (nonce < 0xffffffffffff0000 ? nonce + 1 : 0)
       Block.set(block, {
         nonce: nonce
       })
@@ -67,7 +88,7 @@ function continueMining() {
     }
     
     const duration = helper.unixTimeMs() - timeStart
-    const speed = parseInt(HASHES_PER_CYCLE * 1000 / duration)
+    const speed = parseInt(hashesPerCycle * 1000 / duration)
     console.log('Min.', speed, 'HPS', Block.unpackDiff(header).toString('hex'))
     
     working = false
