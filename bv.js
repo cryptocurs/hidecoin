253639794ee7841d340124454cfaf5331d16b3b4
cryptocoins <crypto-coins@yandex.ru>
2017-03-09 09:32:46 +0300

    Version 0.4.0

--- bv.js
+++ bv.js
@@ -4,9 +4,10 @@ const blockchain = require('./lib/Blockchain')
 const Block = require('./lib/Block')
 const Tx = require('./lib/Tx')
 
-const action = process.argv[2] || process.exit(0)
+var id = process.argv[2] || process.exit(0)
+var toId = null
 
-if (action === 'e') {
+if (id === 'e') {
   let maxId
   if (maxId = process.argv[3]) {
     blockchain.eachTo(maxId, (block) => {
@@ -17,14 +18,30 @@ if (action === 'e') {
       console.log(block.id, block.hash.toString('hex'))
     })
   }
-} else if (action === 'v') {
-  let id
-  if (id = process.argv[3]) {
-    const block = blockchain.get(parseInt(id))
-    block && console.log(block.hash.toString('hex'), block.data.length)
+} else if (id === 'r') {
+  blockchain.removeLast(null, 5)
+} else if (id === '--repair') {
+  let hasErrors = true
+  while (hasErrors) {
+    console.log('Checking blockchain...')
+    const len = blockchain.getLength()
+    console.log('Length:', len)
+    const {hash, data} = blockchain.get(len - 1)
+    console.log('Hash of last block:', hash.toString('hex'))
+    if (hash.equals(Buffer.from('0000000000000000000000000000000000000000000000000000000000000000', 'hex'))) {
+      console.log('Error in blockchain')
+      console.log('Repairing blockchain...')
+      blockchain.removeLast(null, 1)
+    } else if (data[0] === 0) {
+      console.log('Error in blockchain')
+      console.log('Repairing blockchain...')
+      blockchain.removeLast(null, 1)
+    } else {
+      hasErrors = false
+    }
   }
+  console.log('No errors in blockchain')
 }
-
 console.log('Blockchain length:', blockchain.getLength())
 setTimeout(() => {
     process.exit()
--- miner.js
+++ miner.js
@@ -1,5 +1,7 @@
 'use strict'
 
+const moment = require('moment')
+
 const config = require('./config')
 const helper = require('./lib/helper')
 const hours = require('./lib/Hours')
@@ -12,92 +14,101 @@ storage.config.listenRpcPort = 5840
 storage.config.callRpcPort = 5839
 const rpcClient = require('./lib/RpcClient')
 
-hours.sync()
-
-console.log('Miner runned')
-
-var hashesPerCycle = 100000
-var nonce = 0
-var working = false
-
-const args = process.argv
-for (let i = 2; i < args.length; i++) {
-  if ((args[i] === '--hpc') && (args[i + 1] !== undefined)) {
-    hashesPerCycle = args[i + 1]
-  } else if ((args[i] === '--nonce') && (args[i + 1] !== undefined)) {
-    const buffer = Buffer.alloc(8, 0x00)
-    let argNonce = args[i + 1]
-    if (argNonce.length % 2) {
-      argNonce = '0' + argNonce
-    }
-    const nonceBuffer = Buffer.from(argNonce, 'hex')
-    nonceBuffer.copy(buffer, 8 - nonceBuffer.length)
-    nonce = Packet(buffer).unpackNumber64()
-  }
+const log = (...data) => {
+  console.log('[' + moment().format('HH:mm:ss') + ']#', ...data)
 }
 
-const initNonce = nonce
-var hps = 0
-
-console.log('Configuration')
-console.log('HPC     :', hashesPerCycle)
-console.log('Nonce   :', nonce)
-
-function continueMining() {
-  if (working) {
-    console.log('Already mining')
-    return
+hours.sync((err, res) => {
+  if (err) {
+    log('Error while synchronizing time', err)
+    process.exit()
   }
-  working = true
-  console.log('Requesting fresh data')
-  rpcClient.call('getMiningTask', {nonce: initNonce, hps: hps}, (res) => {
-    if (!res || !res.result) {
-      console.log('Request error')
-      setTimeout(continueMining, 1000)
-      working = false
+  
+  log('Miner runned')
+
+  var hashesPerCycle = 100000
+  var nonce = 0
+  var working = false
+
+  const args = process.argv
+  for (let i = 2; i < args.length; i++) {
+    if ((args[i] === '--hpc') && (args[i + 1] !== undefined)) {
+      hashesPerCycle = args[i + 1]
+    } else if ((args[i] === '--nonce') && (args[i + 1] !== undefined)) {
+      const buffer = Buffer.alloc(8, 0x00)
+      let argNonce = args[i + 1]
+      if (argNonce.length % 2) {
+        argNonce = '0' + argNonce
+      }
+      const nonceBuffer = Buffer.from(argNonce, 'hex')
+      nonceBuffer.copy(buffer, 8 - nonceBuffer.length)
+      nonce = Packet(buffer).unpackNumber64()
+    }
+  }
+
+  const initNonce = nonce
+  var hps = 0
+
+  log('Configuration')
+  log('HPC     :', hashesPerCycle)
+  log('Nonce   :', nonce)
+
+  function continueMining() {
+    if (working) {
+      log('Already mining')
       return
     }
-    
-    const result = res.result
-    if (!result.active) {
-      console.log('Mining suspended')
-      hps = 0
-      working = false
-      setTimeout(continueMining, 1000)
-      return
-    }
-    
-    const block = helper.baseToBuf(result.blockData)
-    const header = block.slice(0, result.blockHeaderSize)
-    
-    console.log('Received fresh data, block size', block.length, 'bytes')
-    
-    let hash
-    const timeStart = helper.unixTimeMs()
-    for (let i = 0; i < hashesPerCycle; i++) {
-      nonce = (nonce < 0xffffffffffff0000 ? nonce + 1 : 0)
-      Block.set(block, {
-        nonce: nonce
-      })
-      hash = Block.calcHash(header)
-      if (hash) {
-        console.log('FOUND', hash.toString('hex'))
-        rpcClient.call('blockFound', {hash: helper.bufToBase(hash), blockData: helper.bufToBase(block), txHashList: result.txHashList}, (res) => {
-          res && res.status && console.log(res.status)
-        })
-        setTimeout(continueMining, 500)
+    working = true
+    log('Requesting fresh data')
+    rpcClient.call('getMiningTask', {nonce: initNonce, hps: hps}, (res) => {
+      if (!res || !res.result) {
+        log('Request error')
+        setTimeout(continueMining, 1000)
         working = false
         return
       }
-    }
-    
-    const duration = helper.unixTimeMs() - timeStart
-    hps = parseInt(hashesPerCycle * 1000 / duration)
-    console.log('Min.', hps, 'HPS', Block.unpackDiff(header).toString('hex'))
-    
-    working = false
-    continueMining()
-  })
-}
+      
+      const result = res.result
+      if (!result.active) {
+        log('Mining suspended')
+        hps = 0
+        working = false
+        setTimeout(continueMining, 1000)
+        return
+      }
+      
+      const block = helper.baseToBuf(result.blockData)
+      const header = block.slice(0, result.blockHeaderSize)
+      
+      log('Received fresh data, block size', block.length, 'bytes')
+      
+      let hash
+      const timeStart = helper.unixTimeMs()
+      for (let i = 0; i < hashesPerCycle; i++) {
+        nonce = (nonce < 0xffffffffffff0000 ? nonce + 1 : 0)
+        Block.set(block, {
+          nonce: nonce
+        })
+        hash = Block.calcHash(header)
+        if (hash) {
+          log('FOUND', hash.toString('hex'))
+          rpcClient.call('blockFound', {hash: helper.bufToBase(hash), blockData: helper.bufToBase(block), txHashList: result.txHashList}, (res) => {
+            res && res.status && log(res.status)
+          })
+          setTimeout(continueMining, 500)
+          working = false
+          return
+        }
+      }
+      
+      const duration = helper.unixTimeMs() - timeStart
+      hps = parseInt(hashesPerCycle * 1000 / duration)
+      log('HPS', hps, 'Diff', Block.unpackDiff(header).toString('hex'))
+      
+      working = false
+      continueMining()
+    })
+  }
 
-continueMining()
+  continueMining()
+})
